15cb98ac2fa712f4fe97917de298f2881496c14a
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-06-24 16:33:52 -0500

    Created initial script

---
+++ Capstone.R
@@ -0,0 +1,26 @@
+# Coursera Data Science Specialization Capstone Project Script------------------
+# Predicting the next word in a sequence
+
+
+# The purpose of this script is to complete the basic requirements behind the
+# capstone project from Johns Hopkins University within the Data Science
+# Specialization on Coursera.
+#
+# The instructions outline the objectives of the project as:
+# - Analyze a large corpus of text documents
+# - Discover structure in the data and how words are put together
+# - Clean and analyze the text data
+# - Build a predictive text model
+# - Build a predictive text product that uses the model
+#
+# This project will begin to meet the objectives by building a script to work
+# with the data and build a predictive model
+
+
+#library()
+
+
+# Part 1) Load and process the data---------------------------------------------
+
+
+
