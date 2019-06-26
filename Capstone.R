670ce82718223c1c91ecbb431af176d396368423
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-06-25 21:20:09 -0500

    Experimenting with ways to load and manipulate the data

--- Capstone.R
+++ Capstone.R
@@ -17,7 +17,9 @@
 # with the data and build a predictive model
 
 
+library(tidyverse)
 library(readtext)
+library(quanteda)
 
 
 # Part 0) Function definitions--------------------------------------------------
@@ -50,8 +52,40 @@ DLAndUnzipData <- function() {
 
 # Part 1) Load and process the data---------------------------------------------
 
-test <- DLAndUnzipData()
+# Download and unzip the data, store folder name as object
+data.folder <- DLAndUnzipData()
 
+# Create a small practice file from a real file
+data.folder %>%
+  paste0("/en_US/en_US.twitter.txt") %>%  # Append the subfolder and filename
+  readLines(n = 5) %>%
+  writeLines(con = "final/en_US/jz.txt")
+
+# Create an object to compare to
+test.object <- data.folder %>%
+  paste0("/en_US/en_US.twitter.txt") %>%  # Append the subfolder and filename
+  readLines(n = 5)
+
+# Load in the dataset to experiment
+jz.corp <- data.folder %>%
+  paste0("/en_US/jz.txt") %>%  # Append the subfolder and filename
+  readtext()  %>%  # 1 obs. of 2 variables
+  corpus()  # List of 4
+
+rm(data.folder, DLAndUnzipData)
+
+jz.tkn <- tokens(jz.corp, what = "sentence")
+
+#test <- tokens(en.jz.tkn[[1]][1])
+
+
+
+# Project instructions:
+# Exploratory analysis - perform a thorough exploratory analysis of the data,
+# understanding the distribution of words and relationship between the words in
+# the corpora. Understand frequencies of words and word pairs - build figures
+# and tables to understand variation in the frequencies of words and word pairs
+# in the data.
 
 
 
