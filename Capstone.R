21be0f9b43b951777ff94527ba28dd8ecce99222
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-07-24 18:48:30 -0500

    Changed functionality to ensure the correct number of predictions is always returned

--- Capstone.R
+++ Capstone.R
@@ -69,18 +69,20 @@ prefix.words <- str_split(text.input, pattern = " ") %>%
 #   
 # }
 
-FindWords <- function(ngram.table, prefix.words, discount, order.maximum,
-                      predictions.needed) {
+FindWords <- function(ngram.table, prefix.words, discount, order.maximum) {
+  
+  predictions.desired <- 5L
+  predictions.found <- 0L
   
   # Create empty tables to be updated later
   pred.table.upper <- tibble(feature = character(0), word = character(0),
                              score = numeric(0))
   pred.table.lower <- pred.table.upper
-  
+
   # Truncate input text to maximum length supported by the model
   prefix.words <- tail(prefix.words, order.maximum)
-  order.used <- length(prefix.words)  # Measure length
-  prefix.gram <- paste(prefix.words, collapse = "_")  # Change format
+  order.used <- length(prefix.words)
+  prefix.gram <- paste(prefix.words, collapse = "_")  # Convert to gram format
   
   # Check if preceding gram has been observed
   ngram.vector <- ngram.table %>%
@@ -107,33 +109,33 @@ FindWords <- function(ngram.table, prefix.words, discount, order.maximum,
       mutate(word = completing.words) %>%
       arrange(desc(frequency)) %>%
       ApplyScores() %>%
-      slice(n = 1:5) %>%
+      slice(n = 1:predictions.desired) %>%
       select(feature, word, score)
     
     number.found <- length(completing.grams)
-    predictions.needed <- max(predictions.needed - number.found, 0L)
-    
+
   }
   
-  if (predictions.needed > 0) {
+  if (number.found < predictions.desired) {
     
     order.maximum <- order.maximum - 1L
     
     pred.table.lower <- FindWords(ngram.table, prefix.words, discount,
-                                  order.maximum, predictions.needed)
+                                  order.maximum)
     pred.table.lower <- mutate(pred.table.lower, score = score * discount)
     
   }
   
   prediction.table <- bind_rows(pred.table.upper, pred.table.lower) %>%
     arrange(desc(score)) %>%
-    distinct(word, .keep_all = TRUE)
+    distinct(word, .keep_all = TRUE) %>%
+    slice(n = 1:predictions.desired)
   
   return(prediction.table)
   
 }
 
-prediction <- FindWords(ngrams, prefix.words, alpha.value, 2L, 3L)
+prediction <- FindWords(ngrams, prefix.words, alpha.value, 2L)
 print(prediction)
 
 # need to do something about duplicate words
